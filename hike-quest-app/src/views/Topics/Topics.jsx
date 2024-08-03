c8f9eb1a32e7488c8167bbc45c9e0b0100b72b57
Martina Beleva <martina_bel@abv.bg>
2024-08-03 10:36:23 +0300

    create Post & all posts functionality

--- hike-quest-app/src/views/Topics/Topics.jsx
+++
@@ -1,7 +0,0 @@
-export default function Topics() {
-    return (
-        <div style={{
-            fontSize: '16px', fontWeight: 'bold',
-            color: '#273f17',   display: 'flex',}}>Topics</div>
-    )
-}
