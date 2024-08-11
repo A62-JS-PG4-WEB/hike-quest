3c2cc9c6b5959a4fe459c4b9471e7ffa6d7ac6bb
Martina Beleva <martina_bel@abv.bg>
2024-08-12 01:23:20 +0300

    css landing page

--- hike-quest-app/src/views/LandingPage/LandingPage.jsx
+++ hike-quest-app/src/views/LandingPage/LandingPage.jsx
@@ -2,19 +2,17 @@ import { useEffect, useState } from "react";
 import { getAllThreads } from "../../services/threads.service";
 import { useNavigate } from "react-router-dom";
 import { COUNT_THREADS_LANDINGPAGE, MAX_CONTENT_TO_SHOW, MIN_CONTENT_TO_SHOW } from "../../common/constants";
+import styles from "./LandingPage.module.css"
 
 export default function LandingPage() {
     const [threads, setThreads] = useState([]);
-    const navigate = useNavigate()
-
+    const navigate = useNavigate();
 
     useEffect(() => {
         const loadThreads = async () => {
             try {
                 const threads = await getAllThreads('', 'date', '');
                 setThreads(threads);
-
-
             } catch (error) {
                 alert(error.message);
             }
@@ -25,43 +23,52 @@ export default function LandingPage() {
 
     const sortedThreads = [...threads].sort((a, b) => b.commentCount - a.commentCount);
 
-    console.log(threads);
-
     return (
-        <div >
-            <div>
-                {threads.length > 0 ? (
-                    <div>
-                        <h1 >Our most pertinent Threads</h1>
-                        {sortedThreads.slice(0, COUNT_THREADS_LANDINGPAGE)
-                            .map(t => (
-                                <p key={t.id}>
-                                    <strong>{t.title}</strong>
-
-                                    <br /><br />{t.content.slice(MIN_CONTENT_TO_SHOW, MAX_CONTENT_TO_SHOW)}...
-                                    <button onClick={() => navigate(`/login`)}>See more</button>
-                                </p>
-                            ))}
-                    </div>
-                ) : (
-                    'Join us to share your most awesome experience!'
-                )}
-
-            </div>
+        <div className={styles.container}>
             {threads.length > 0 ? (
-                <div>
-                    <h1>Newest Threads</h1>
-                    {threads.slice(0, COUNT_THREADS_LANDINGPAGE).map(t => (
-                        <p key={t.id}>
-                            <strong>{t.title}</strong>
-                            <br /><br />{t.content.slice(0, 300)}...
-                            <button onClick={() => navigate(`/login`)}>See more</button>
-                        </p>
-                    ))}
-                </div>
+                <>
+                    <div>
+                        <h1 className={styles.title}>Our most pertinent Threads</h1>
+                        {sortedThreads.slice(0, COUNT_THREADS_LANDINGPAGE).map(t => (
+                            <div key={t.id} className={styles.threadItem}>
+                                <p className={styles.threadHeader}>
+                                    <strong>{t.title}</strong>
+                                   
+                                </p>
+                                <p className='threadDate'> {new Date(t.createdOn).toDateString()}</p>
+                                                    <p className={styles.threadContent}>
+                                    {t.content.slice(MIN_CONTENT_TO_SHOW, MAX_CONTENT_TO_SHOW)}...
+                                </p>
+                                <div className={styles.threadButtons}>
+                                    <button className={styles.button} onClick={() => navigate(`/login`)}>See more</button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div>
+                        <h1 className={styles.title}>Newest Threads</h1>
+                        {threads.slice(0, COUNT_THREADS_LANDINGPAGE).map(t => (
+                            <div key={t.id} className={styles.threadItem}>
+                                <p className={styles.threadHeader}>
+                                    <strong>{t.title}</strong>
+                                </p>
+                                <p className='threadDate'> {new Date(t.createdOn).toDateString()}</p>
+                                <p className={styles.threadContent}>
+                                    {t.content.slice(0, 300)}...
+                                </p>
+                                <div className={styles.threadButtons}>
+                                    <button className={styles.button} onClick={() => navigate(`/login`)}>See more</button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </>
             ) : (
-                'Vast community of great experiences!'
+                <p className={styles.threadStats}>
+                    Join us to share your most awesome experience!
+                </p>
             )}
         </div>
-    )
+    );
 }
